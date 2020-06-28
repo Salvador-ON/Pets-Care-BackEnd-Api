56ddd57f87e21831e8f38bc713582c227892432a
Salvador <salvador.on@outlook.com>
2020-06-28 17:58:29 -0300

    validate service model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :service
+  validates_presence_of :user_id, :service_id, :date, :time
+
+  validates :pet_name, presence: true, length: {maximum: 25}
 end
--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,8 @@
 class Service < ApplicationRecord
   has_many :appointments
+  validates_presence_of :price, :image_url, :schedule
+
+  validates :name, presence: true, length: {maximum: 30}
+
+  validates :description, presence: true, length: {maximum: 250}
 end
