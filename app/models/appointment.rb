bb6a83b1054ad7e7600ac3eb6111d6d05859416b
Salvador <salvador.on@outlook.com>
2020-07-02 10:54:49 -0300

    modify scope

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -5,10 +5,10 @@ class Appointment < ApplicationRecord
 
   validates :pet_name, presence: true, length: { maximum: 25 }
 
-  appointments_select = 'appointments.id, appointments.date, appointments.pet_name,'
-  users_select = ' users.name, users.phone, users.id as user_id'
+  appointments_select = 'appointments.id, appointments.date, appointments.pet_name, appointments.time,'
+  users_select = ' users.name, users.phone, users.id as user_id, services.name as service_name '
 
-  scope :mine, ->(cu_uid) { where(user_id: cu_uid).joins(:user).select(appointments_select + users_select) }
+  scope :mine, ->(cu_uid) { where(user_id: cu_uid).joins(:user).joins(:service).select(appointments_select + users_select) }
 
   scope :availables, ->(s_id, dt) { where(service_id: s_id, date: dt).select('appointments.id', 'appointments.time') }
 
