6f7f4152bf35733fac8294d1e3eff932031419dd
Salvador <salvador.on@outlook.com>
2020-07-04 18:28:19 -0300

    fix sessions cokie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,5 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://pets-care-api.herokuapp.com/" #app back end
+if Rails.env == "production"
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "https://pets-care-api.herokuapp.com/" #app back end
 else
   Rails.application.config.session_store :cookie_store, key: "_authentication_app"
 end   
