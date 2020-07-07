c23472600094313d3db23bc4e7f601d2a41bacc6
Salvador <salvador.on@outlook.com>
2020-07-07 13:43:05 -0300

    add more testcases

--- spec/requests/api_sessions_spec.rb
+++ spec/requests/api_sessions_spec.rb
@@ -2,16 +2,38 @@ require 'rails_helper'
 
 RSpec.describe 'test api sessions routes', type: :request do
   it 'return success if get /logged_in is valid ' do
+    User.create(email: 'ut1@ut1.com',
+                name: 'user test 1',
+                phone: '123456789',
+                password: '123456',
+                password_confirmation: '123456',
+                role: 0)
+    post '/signin', params: { user: { email: 'ut1@ut1.com', password: '123456' } }
     get '/logged_in'
     expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body)['logged_in']).to eq(true)
   end
 
   it 'return success if delete /logout is valid ' do
+    User.create(email: 'ut1@ut1.com',
+                name: 'user test 1',
+                phone: '123456789',
+                password: '123456',
+                password_confirmation: '123456',
+                role: 0)
+    post '/signin', params: { user: { email: 'ut1@ut1.com', password: '123456' } }
     delete '/logout'
     expect(response).to have_http_status(:success)
+    expect(JSON.parse(response.body)['logged_out']).to eq(true)
   end
 
   it 'returns false if user is not logged_in' do
+    User.create(email: 'ut1@ut1.com',
+                name: 'user test 1',
+                phone: '123456789',
+                password: '123456',
+                password_confirmation: '123456',
+                role: 0)
     get '/logged_in'
     expect(JSON.parse(response.body)['logged_in']).to eq(false)
   end
